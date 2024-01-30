4028ff10f252def4290f52c94015df3284a92426
FernandoTorres <torrestovarfernandojose@gmail.com>
2024-01-30 11:54:12 -0600

    Agregacion de tablas de ventas, serie, tractores, y pruebas del formulario de ventas

--- mavepo.sql
+++ mavepo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-01-2024 a las 20:28:27
+-- Tiempo de generación: 30-01-2024 a las 18:36:42
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -41,12 +41,31 @@ INSERT INTO `roles` (`id_rol`, `nombre_rol`) VALUES
 (2, 'Lectura'),
 (3, 'Usuario Regular');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `serie`
+--
+
+CREATE TABLE `serie` (
+  `id_serie` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `tipo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `serie`
+--
+
+INSERT INTO `serie` (`id_serie`, `nombre`, `tipo`) VALUES
+(123, 'Serie A', 'Tractor de granja');
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `solicitudes`
 --
+
 CREATE TABLE `solicitudes` (
   `id` int(99) NOT NULL,
   `nombreSolicitud` varchar(99) NOT NULL,
@@ -62,10 +81,28 @@ CREATE TABLE `solicitudes` (
 -- Volcado de datos para la tabla `solicitudes`
 --
 
-INSERT INTO `solicitudes` (`id`, `nombreSolicitud`, `responsable`, `fecha`, `estado`, `presupuesto`, `prioridad`) VALUES
-(1, 'Cotización pc', 'Luis', '25/01/2024', 'En curso', 1200, 'Media'),
-(2, 'Papeleria', 'Juan', '25/01/2024', 'Detenido', 5000, 'Alta'),
-(3, 'piezas', 'Juan', '24/01/2024', 'Listo', 500, 'Baja');
+INSERT INTO `solicitudes` (`id`, `nombreSolicitud`, `descripcion`, `responsable`, `fecha`, `estado`, `presupuesto`, `prioridad`) VALUES
+(1, 'Cotización pc', '', 'Luis', '25/01/2024', 'En curso', 1200, 'Media'),
+(2, 'Papeleria', '', 'Juan', '25/01/2024', 'Detenido', 5000, 'Alta'),
+(3, 'piezas', '', 'Juan', '24/01/2024', 'Listo', 500, 'Baja');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tractores`
+--
+
+CREATE TABLE `tractores` (
+  `id` int(11) NOT NULL,
+  `id_serie` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `tractores`
+--
+
+INSERT INTO `tractores` (`id`, `id_serie`) VALUES
+(1, 123);
 
 -- --------------------------------------------------------
 
@@ -90,6 +127,27 @@ INSERT INTO `usuarios` (`id`, `usuario`, `contrasena`, `nombre`, `sucursal`, `id
 (1, 'Sistemas', 'Mavepo217', 'Sistemas', 'SLP', 1),
 (2, 'admin', 'admin', 'admin', 'SLP', 2);
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas`
+--
+
+CREATE TABLE `ventas` (
+  `id_venta` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_tractor` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `ventas`
+--
+
+INSERT INTO `ventas` (`id_venta`, `id_usuario`, `id_tractor`, `cantidad`, `fecha`) VALUES
+(1, 1, 1, 2, '2024-01-30');
+
 --
 -- Índices para tablas volcadas
 --
@@ -101,11 +159,32 @@ ALTER TABLE `roles`
   ADD PRIMARY KEY (`id_rol`);
 
 --
+-- Indices de la tabla `serie`
+--
+ALTER TABLE `serie`
+  ADD PRIMARY KEY (`id_serie`);
+
+--
+-- Indices de la tabla `tractores`
+--
+ALTER TABLE `tractores`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_serie` (`id_serie`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_rol` (`id_rol`);
+
+--
+-- Indices de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`id_venta`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_tractor` (`id_tractor`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -121,17 +200,36 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
+-- Filtros para la tabla `tractores`
+--
+ALTER TABLE `tractores`
+  ADD CONSTRAINT `tractores_ibfk_1` FOREIGN KEY (`id_serie`) REFERENCES `serie` (`id_serie`);
+
+--
 -- Filtros para la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`);
+
+--
+-- Filtros para la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `ventas_ibfk_2` FOREIGN KEY (`id_tractor`) REFERENCES `tractores` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
